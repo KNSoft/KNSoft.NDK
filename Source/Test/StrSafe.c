f1767569f002b861224267f516d46f1cc85ee4a6
Ratin Gao <ratin@knsoft.org>
2024-05-09 15:35:56 +0800

    [UNITTEST:STRSAFE] Code improvements

--- Source/Test/StrSafe.c
+++ Source/Test/StrSafe.c
@@ -6,27 +6,31 @@
 
 #include <KNSoft/NDK/Extension/StrSafe.h>
 
-#define TEST_STRING1 "KNSoft.NDK StrSafe.h Test String"
-#define TEST_STRING1_PART1 "KNSoft.NDK StrSafe.h Test"
+#define DEFINE_TEST_STRING(Name, String)\
+static const char Name##_A[] = String;\
+static const wchar_t Name##_W[] = _A2W(String);
+
+DEFINE_TEST_STRING(TEST_STRING1, "KNSoft.NDK StrSafe.h Test String");
+DEFINE_TEST_STRING(TEST_STRING1_PART1, "KNSoft.NDK StrSafe.h Test");
 
 TEST_DECL(StrSafeFunc)
 {
-    char szTempA[_countof(TEST_STRING1)];
-    wchar_t szTempW[_countof(TEST_STRING1)];
+    char szTempA[_countof(TEST_STRING1_A)];
+    wchar_t szTempW[_countof(TEST_STRING1_W)];
 
-    TEST_OK(StrSafe_CchPrintfA(NULL, 0, "%hs", TEST_STRING1) == _STR_CCH_LEN(TEST_STRING1));
-    TEST_OK(StrSafe_CchPrintfA(szTempA, ARRAYSIZE(szTempA), "%hs", TEST_STRING1) == _STR_CCH_LEN(TEST_STRING1));
-    TEST_OK(strcmp(szTempA, TEST_STRING1) == 0);
-    TEST_OK(StrSafe_CchPrintfW(NULL, 0, L"%ls", _A2W(TEST_STRING1)) == _STR_CCH_LEN(TEST_STRING1));
-    TEST_OK(StrSafe_CchPrintfW(szTempW, ARRAYSIZE(szTempW), L"%ls", _A2W(TEST_STRING1)) == _STR_CCH_LEN(TEST_STRING1));
-    TEST_OK(wcscmp(szTempW, _A2W(TEST_STRING1)) == 0);
+    TEST_OK(StrSafe_CchPrintfA(NULL, 0, "%hs", TEST_STRING1_A) == _STR_CCH_LEN(TEST_STRING1_A));
+    TEST_OK(StrSafe_CchPrintfA(szTempA, ARRAYSIZE(szTempA), "%hs", TEST_STRING1_A) == _STR_CCH_LEN(TEST_STRING1_A));
+    TEST_OK(strcmp(szTempA, TEST_STRING1_A) == 0);
+    TEST_OK(StrSafe_CchPrintfW(NULL, 0, L"%ls", TEST_STRING1_W) == _STR_CCH_LEN(TEST_STRING1_W));
+    TEST_OK(StrSafe_CchPrintfW(szTempW, ARRAYSIZE(szTempW), L"%ls", TEST_STRING1_W) == _STR_CCH_LEN(TEST_STRING1_W));
+    TEST_OK(wcscmp(szTempW, TEST_STRING1_W) == 0);
 
-    TEST_OK(StrSafe_CchPrintfA(szTempA, ARRAYSIZE(szTempA), "%hs", TEST_STRING1_PART1) == _STR_CCH_LEN(TEST_STRING1_PART1));
-    TEST_OK(strcmp(szTempA, TEST_STRING1_PART1) == 0);
-    TEST_OK(StrSafe_CchPrintfA(szTempA, ARRAYSIZE(szTempA), "%hs$", TEST_STRING1) == _STR_CCH_LEN(TEST_STRING1) + 1);
-    TEST_OK(strcmp(szTempA, TEST_STRING1) == 0);
-    TEST_OK(StrSafe_CchPrintfW(szTempW, ARRAYSIZE(szTempW), L"%ls", _A2W(TEST_STRING1_PART1)) == _STR_CCH_LEN(TEST_STRING1_PART1));
-    TEST_OK(wcscmp(szTempW, _A2W(TEST_STRING1_PART1)) == 0);
-    TEST_OK(StrSafe_CchPrintfW(szTempW, ARRAYSIZE(szTempW), L"%ls$", _A2W(TEST_STRING1)) == _STR_CCH_LEN(TEST_STRING1) + 1);
-    TEST_OK(wcscmp(szTempW, _A2W(TEST_STRING1)) == 0);
+    TEST_OK(StrSafe_CchPrintfA(szTempA, ARRAYSIZE(szTempA), "%hs", TEST_STRING1_PART1_A) == _STR_CCH_LEN(TEST_STRING1_PART1_A));
+    TEST_OK(strcmp(szTempA, TEST_STRING1_PART1_A) == 0);
+    TEST_OK(StrSafe_CchPrintfA(szTempA, ARRAYSIZE(szTempA), "%hs$", TEST_STRING1_A) == _STR_CCH_LEN(TEST_STRING1_A) + 1);
+    TEST_OK(strcmp(szTempA, TEST_STRING1_A) == 0);
+    TEST_OK(StrSafe_CchPrintfW(szTempW, ARRAYSIZE(szTempW), L"%ls", TEST_STRING1_PART1_W) == _STR_CCH_LEN(TEST_STRING1_PART1_W));
+    TEST_OK(wcscmp(szTempW, TEST_STRING1_PART1_W) == 0);
+    TEST_OK(StrSafe_CchPrintfW(szTempW, ARRAYSIZE(szTempW), L"%ls$", TEST_STRING1_W) == _STR_CCH_LEN(TEST_STRING1_W) + 1);
+    TEST_OK(wcscmp(szTempW, TEST_STRING1_W) == 0);
 }
