5d24216268e53e88a74982788132a7d17dda97b0
Ratin Gao <ratin@knsoft.org>
2024-06-11 08:15:34 +0800

    [RTL] Add more bitmap functions

--- Source/Include/KNSoft/NDK/NT/API/Rtl.Bitmap.h
+++ Source/Include/KNSoft/NDK/NT/API/Rtl.Bitmap.h
@@ -13,4 +13,96 @@ RtlInitializeBitMap(
     _In_opt_ __drv_aliasesMem PULONG BitMapBuffer,
     _In_opt_ ULONG SizeOfBitMap);
 
+NTSYSAPI
+VOID
+NTAPI
+RtlClearBit(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_range_(<, BitMapHeader->SizeOfBitMap) ULONG BitNumber);
+
+NTSYSAPI
+VOID
+NTAPI
+RtlSetBit(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_range_(<, BitMapHeader->SizeOfBitMap) ULONG BitNumber);
+
+_Must_inspect_result_
+NTSYSAPI
+BOOLEAN
+NTAPI
+RtlTestBit(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_range_(<, BitMapHeader->SizeOfBitMap) ULONG BitNumber);
+
+NTSYSAPI
+VOID
+NTAPI
+RtlClearAllBits(
+    _In_ PRTL_BITMAP BitMapHeader);
+
+NTSYSAPI
+VOID
+NTAPI
+RtlSetAllBits(
+    _In_ PRTL_BITMAP BitMapHeader);
+
+_Success_(return != -1)
+_Ret_range_(<=, BitMapHeader->SizeOfBitMap - NumberToFind)
+_Must_inspect_result_
+NTSYSAPI
+ULONG
+NTAPI
+RtlFindClearBits(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_ ULONG NumberToFind,
+    _In_ ULONG HintIndex);
+
+_Success_(return != -1)
+_Ret_range_(<=, BitMapHeader->SizeOfBitMap - NumberToFind)
+_Must_inspect_result_
+NTSYSAPI
+ULONG
+NTAPI
+RtlFindSetBits(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_ ULONG NumberToFind,
+    _In_ ULONG HintIndex);
+
+_Success_(return != -1)
+_Ret_range_(<=, BitMapHeader->SizeOfBitMap - NumberToFind)
+NTSYSAPI
+ULONG
+NTAPI
+RtlFindClearBitsAndSet(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_ ULONG NumberToFind,
+    _In_ ULONG HintIndex);
+
+_Success_(return != -1)
+_Ret_range_(<=, BitMapHeader->SizeOfBitMap - NumberToFind)
+NTSYSAPI
+ULONG
+NTAPI
+RtlFindSetBitsAndClear(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_ ULONG NumberToFind,
+    _In_ ULONG HintIndex);
+
+NTSYSAPI
+VOID
+NTAPI
+RtlClearBits(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_range_(0, BitMapHeader->SizeOfBitMap - NumberToClear) ULONG StartingIndex,
+    _In_range_(0, BitMapHeader->SizeOfBitMap - StartingIndex) ULONG NumberToClear);
+
+NTSYSAPI
+VOID
+NTAPI
+RtlSetBits(
+    _In_ PRTL_BITMAP BitMapHeader,
+    _In_range_(0, BitMapHeader->SizeOfBitMap - NumberToSet) ULONG StartingIndex,
+    _In_range_(0, BitMapHeader->SizeOfBitMap - StartingIndex) ULONG NumberToSet);
+
 EXTERN_C_END
