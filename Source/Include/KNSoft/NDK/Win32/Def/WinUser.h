6779978fef7313555c8822be9edcff934543bb04
Ratin Gao <ratin@knsoft.org>
2024-09-26 23:13:38 +0800

    [WINUSER] Add `DLGTEMPLATEEX` and `DLGITEMTEMPLATEEX`

--- Source/Include/KNSoft/NDK/Win32/Def/WinUser.h
+++ Source/Include/KNSoft/NDK/Win32/Def/WinUser.h
@@ -1,5 +1,21 @@
 ﻿#pragma once
 
+#include <WinUser.h>
+
 #ifndef WM_COPYGLOBALDATA
 #define WM_COPYGLOBALDATA 0x49
 #endif
+
+typedef struct _DLGTEMPLATEEX
+{
+    WORD        dlgVer;
+    WORD        signature;
+    DWORD       helpID;
+    DLGTEMPLATE dlgTemplate;
+} DLGTEMPLATEEX, *PDLGTEMPLATEEX;
+
+DECLSPEC_ALIGN(4) typedef struct _DLGITEMTEMPLATEEX
+{
+    DWORD           helpID;
+    DLGITEMTEMPLATE itemTemplate;
+} DLGITEMTEMPLATEEX, *PDLGITEMTEMPLATEEX;
