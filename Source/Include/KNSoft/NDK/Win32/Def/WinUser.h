993d3bbeb3fc2de4e1d0bebfeb18ec6de7e88362
Ratin Gao <ratin@knsoft.org>
2024-09-30 03:14:23 +0800

    [WINUSER] Add more resource file structures

--- Source/Include/KNSoft/NDK/Win32/Def/WinUser.h
+++ Source/Include/KNSoft/NDK/Win32/Def/WinUser.h
@@ -6,6 +6,119 @@
 #define WM_COPYGLOBALDATA 0x49
 #endif
 
+#pragma region Resource File
+
+typedef struct _CURSORDIR
+{
+    WORD Width;
+    WORD Height;
+} CURSORDIR, *PCURSORDIR;
+
+typedef struct _DIRENTRY
+{
+    WORD fontOrdinal;
+} DIRENTRY, *PDIRENTRY;
+
+typedef struct _FONTDIRENTRY
+{
+    WORD  dfVersion;
+    DWORD dfSize;
+    CHAR  dfCopyright[60];
+    WORD  dfType;
+    WORD  dfPoints;
+    WORD  dfVertRes;
+    WORD  dfHorizRes;
+    WORD  dfAscent;
+    WORD  dfInternalLeading;
+    WORD  dfExternalLeading;
+    BYTE  dfItalic;
+    BYTE  dfUnderline;
+    BYTE  dfStrikeOut;
+    WORD  dfWeight;
+    BYTE  dfCharSet;
+    WORD  dfPixWidth;
+    WORD  dfPixHeight;
+    BYTE  dfPitchAndFamily;
+    WORD  dfAvgWidth;
+    WORD  dfMaxWidth;
+    BYTE  dfFirstChar;
+    BYTE  dfLastChar;
+    BYTE  dfDefaultChar;
+    BYTE  dfBreakChar;
+    WORD  dfWidthBytes;
+    DWORD dfDevice;
+    DWORD dfFace;
+    DWORD dfReserved;
+    CHAR  szDeviceName[];
+    // CHAR  szFaceName[];
+} FONTDIRENTRY, *PFONTDIRENTRY;
+
+typedef struct _FONTGROUPHDR
+{
+    WORD     NumberOfFonts;
+    DIRENTRY DE[];
+} FONTGROUPHDR, *PFONTGROUPHDR;
+
+typedef struct _ICONRESDIR
+{
+    BYTE Width;
+    BYTE Height;
+    BYTE ColorCount;
+    BYTE reserved;
+} ICONRESDIR, *PICONRESDIR;
+
+typedef struct _LOCALHEADER
+{
+    WORD xHotSpot;
+    WORD yHotSpot;
+} LOCALHEADER, *PLOCALHEADER;
+
+typedef struct _MENUHEADER
+{
+    WORD wVersion;
+    WORD cbHeaderSize;
+} MENUHEADER, *PMENUHEADER;
+
+typedef struct _MENUHELPID
+{
+    DWORD helpID;
+} MENUHELPID, *PMENUHELPID;
+
+typedef struct _NEWHEADER
+{
+    WORD Reserved;
+    WORD ResType;
+    WORD ResCount;
+} NEWHEADER, *PNEWHEADER;
+
+typedef struct _NORMALMENUITEM
+{
+    WORD    resInfo;
+    WCHAR   menuText[]; // szOrOrd
+} NORMALMENUITEM, *PNORMALMENUITEM;
+
+typedef struct _POPUPMENUITEM
+{
+    DWORD   type;
+    DWORD   state;
+    DWORD   id;
+    WORD    resInfo;
+    WCHAR   menuText[]; // szOrOrd
+} POPUPMENUITEM, *PPOPUPMENUITEM;
+
+typedef struct _RESDIR
+{
+  union
+  {
+      ICONRESDIR   Icon;
+      CURSORDIR    Cursor;
+  };
+  WORD       Planes;
+  WORD       BitCount;
+  DWORD      BytesInRes;
+  WORD       IconCursorId;
+} RESDIR, *PRESDIR;
+
 typedef struct _DLGTEMPLATEEX
 {
     WORD        dlgVer;
@@ -19,3 +132,5 @@ DECLSPEC_ALIGN(4) typedef struct _DLGITEMTEMPLATEEX
     DWORD           helpID;
     DLGITEMTEMPLATE itemTemplate;
 } DLGITEMTEMPLATEEX, *PDLGITEMTEMPLATEEX;
+
+#pragma endregion
